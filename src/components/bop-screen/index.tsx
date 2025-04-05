7cc3aa6951dd63fd00127faa0778992af40486ed
siddhant kaushik <siddhant.kaushik@impronics.co.za>
2025-04-05 18:49:01 +0530

    fixes

--- src/components/bop-screen/index.tsx
+++ src/components/bop-screen/index.tsx
@@ -133,7 +133,7 @@ const BopScreen: React.FC = () => {
       .then((response) => response.json())
       .then((result) => {
         console.log(result, 'payal')
-        // window.location.reload()
+        window.location.reload()
       })
       .catch((error) => console.error(error))
   }
