24304038e66bdfdba42f8ba45dd5942c3aa9d59a
payal.garg <garg.payal@impronics.co.za>
2025-07-07 15:03:49 +0530

    added credited transactions api

--- src/components/referralTransactionTable/index.tsx
+++ src/components/referralTransactionTable/index.tsx
@@ -1,185 +1,165 @@
-import react, { useState } from 'react';
-import { DataGrid } from '@mui/x-data-grid';
-import { Box, Button, Dialog, DialogContent, DialogTitle, DialogActions } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import react, { useState } from 'react'
+import { DataGrid } from '@mui/x-data-grid'
+import { Box, Button, Dialog, DialogContent, DialogTitle, DialogActions } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 //@ts-ignore
 const ReferralDataGrid = ({ rows, columns, width }) => {
-    return (<DataGrid
-        sx={{
-            width: width,
-            '& .MuiDataGrid-columnHeaders': {
-                '& .super-app-theme--header': {
-                    backgroundColor: '#005099',
-                    color: 'white',
-                },
-            },
-            '& .MuiDataGrid-columnHeaderTitle': {
-                fontWeight: 'bold',
-            },
-            '& .MuiDataGrid-cell': {
-                fontSize: '14px',
-            },
-            '& .MuiDataGrid-row:nth-of-type(even)': {
-                backgroundColor: '#f0f8ff',
-            },
-            '& .MuiDataGrid-row:nth-of-type(odd)': {
-                backgroundColor: '#ffffff',
-            },
-            '& .super-app-theme--header': {
-                fontSize: '16px',
-            },
-        }}
-        columns={columns}
-        rows={rows || []}
-        //@ts-ignore
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        disableSelectionOnClick
-    />)
+  return (
+    <DataGrid
+      sx={{
+        width: width,
+        '& .MuiDataGrid-columnHeaders': {
+          '& .super-app-theme--header': {
+            backgroundColor: '#005099',
+            color: 'white',
+          },
+        },
+        '& .MuiDataGrid-columnHeaderTitle': {
+          fontWeight: 'bold',
+        },
+        '& .MuiDataGrid-cell': {
+          fontSize: '14px',
+        },
+        '& .MuiDataGrid-row:nth-of-type(even)': {
+          backgroundColor: '#f0f8ff',
+        },
+        '& .MuiDataGrid-row:nth-of-type(odd)': {
+          backgroundColor: '#ffffff',
+        },
+        '& .super-app-theme--header': {
+          fontSize: '16px',
+        },
+      }}
+      columns={columns}
+      rows={rows || []}
+      //@ts-ignore
+      pageSize={5}
+      rowsPerPageOptions={[5]}
+      disableSelectionOnClick
+      getRowId={(row) => row.referreeId || row.id}
+    />
+  )
 }
 
-
 const ReferralTransactions = ({
-    //@ts-ignore
-    referralRecords, referralType
+  //@ts-ignore
+  referralRecords,
+  //@ts-ignore
+  referralType,
 }) => {
-    const [showTransactionModal, setShowTransactionModal] = useState<boolean>(false);
-    const [transactionList, setTransactionList] = useState<any>([]);
-    const navigate = useNavigate();
+  const [showTransactionModal, setShowTransactionModal] = useState<boolean>(false)
+  const [transactionList, setTransactionList] = useState<any>([])
+  const navigate = useNavigate()
 
-    const ReferralColumns = [
-        {
-            field: 'countryCode',
-            headerName: 'Country Code',
-            flex: 1,
-            headerClassName: 'super-app-theme--header',
-        },
-        {
-            field: 'transactionNumber',
-            headerName: 'Transaction Number',
-            flex: 1,
-            headerClassName: 'super-app-theme--header',
-        },
-        {
-            field: 'rewardRedeemed',
-            headerName: referralType === 'Credited' ? 'Reward Credited' : 'Reward Redeemed',
-            flex: 1,
-            headerClassName: 'super-app-theme--header',
-        },
-        {
-            field: 'referrerApplicantId',
-            headerName: 'Applicant Id',
-            flex: 1,
-            headerClassName: 'super-app-theme--header',
-            // renderCell: (params: any) => (
-            //     <span
-            //         style={{ color: '#1976d2', cursor: 'pointer' }}
-            //         onClick={() => navigate(`/applicant-details/${params.value}`)}
-            //     >
-            //         {params.value}
-            //     </span>
-            // ),
-        },
+  const ReferralColumns = [
+    {
+      field: 'countryCode',
+      headerName: 'Country Code',
+      flex: 1,
+      headerClassName: 'super-app-theme--header',
+    },
+    {
+      field: 'transactionNumber',
+      headerName: 'Transaction Number',
+      flex: 1,
+      headerClassName: 'super-app-theme--header',
+    },
+    {
+      field: 'rewardRedeemed',
+      headerName: referralType === 'Credited' ? 'Reward Credited' : 'Reward Redeemed',
+      flex: 1,
+      headerClassName: 'super-app-theme--header',
+    },
+    {
+      field: 'referrerApplicantId',
+      headerName: 'Applicant Id',
+      flex: 1,
+      headerClassName: 'super-app-theme--header',
+      // renderCell: (params: any) => (
+      //     <span
+      //         style={{ color: '#1976d2', cursor: 'pointer' }}
+      //         onClick={() => navigate(`/applicant-details/${params.value}`)}
+      //     >
+      //         {params.value}
+      //     </span>
+      // ),
+    },
+  ]
+  const ReferralCreditedColumns = [
+    {
+      field: 'countryCode',
+      headerName: 'Country Code',
+      flex: 1,
+      headerClassName: 'super-app-theme--header',
+    },
+    {
+      field: 'applicantReferralTransaction',
+      headerName: 'Transactions Count',
+      flex: 1,
+      headerClassName: 'super-app-theme--header',
+      renderCell: (params: any) => (
+        <span
+          style={{
+            cursor: 'pointer',
+            color: '#1976d2',
+          }}
+          onClick={() => {
+            setShowTransactionModal(!showTransactionModal)
+            setTransactionList(params.row.applicantReferralTransaction)
+          }}
+        >
+          {params.row.applicantReferralTransaction.length}
+        </span>
+      ),
+    },
+    {
+      field: 'totalRewards',
+      headerName: 'Total Rewards',
+      flex: 1,
+      headerClassName: 'super-app-theme--header',
+    },
+    {
+      field: 'referreeId',
+      headerName: 'Applicant Id',
+      flex: 1,
+      headerClassName: 'super-app-theme--header',
+      // renderCell: (params: any) => (
+      //     <span
+      //         style={{ color: '#1976d2', cursor: 'pointer' }}
+      //         onClick={() => navigate(`/applicant-details/${params.value}`)}
+      //     >
+      //         {params.value}
+      //     </span>
+      // ),
+    },
+  ]
 
-    ]
-    const ReferralCreditedColumns = [
-        {
-            field: 'countryCode',
-            headerName: 'Country Code',
-            flex: 1,
-            headerClassName: 'super-app-theme--header',
-        },
-        {
-            field: 'transactions',
-            headerName: 'Transactions Count',
-            flex: 1,
-            headerClassName: 'super-app-theme--header',
-            renderCell: (params: any) => (
-                <span style={{
-                    cursor: 'pointer',
-                    color: '#1976d2'
-                }} onClick={() => {
-                    setShowTransactionModal(!showTransactionModal)
-                    setTransactionList(params.row.transactions)
-                }}>{params.row.transactions.length}
-                </span>
-            ),
-        },
-        {
-            field: 'totalReward',
-            headerName: 'Total Reward',
-            flex: 1,
-            headerClassName: 'super-app-theme--header',
-        },
-        {
-            field: 'referrerApplicantId',
-            headerName: 'Applicant Id',
-            flex: 1,
-            headerClassName: 'super-app-theme--header',
-            // renderCell: (params: any) => (
-            //     <span
-            //         style={{ color: '#1976d2', cursor: 'pointer' }}
-            //         onClick={() => navigate(`/applicant-details/${params.value}`)}
-            //     >
-            //         {params.value}
-            //     </span>
-            // ),
-        },
-    ]
+  const handleModalClose = () => {
+    setShowTransactionModal(!setShowTransactionModal)
+  }
 
-    const mapGroupedData = (data: any) => {
-        //@ts-ignore
-        const grouped: any = {};
-        data.forEach((item: any, index: any) => {
-            const key = item.referrerApplicantId;
-            const reward = parseFloat(item.rewardRedeemed);
+  return (
+    <Box>
+      {referralRecords && referralRecords.length > 0 ? (
+        <ReferralDataGrid rows={referralRecords} columns={referralType === 'Credited' ? ReferralCreditedColumns : ReferralColumns} width={'70vw'} />
+      ) : (
+        <p>No Referral Found</p>
+      )}
 
-            if (!grouped[key]) {
-                grouped[key] = {
-                    countryCode: item.countryCode,
-                    referrerApplicantId: item.referrerApplicantId,
-                    totalReward: 0,
-                    transactions: [],
-                    id: index
-                };
-            }
+      <Dialog open={showTransactionModal} onClose={handleModalClose} fullWidth maxWidth="md">
+        <DialogTitle>All Transactions</DialogTitle>
+        <DialogContent>
+          <div style={{ height: 400, width: '800', marginTop: 16 }}>
+            <ReferralDataGrid rows={transactionList} width={'600'} columns={ReferralColumns} />
+          </div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleModalClose}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  )
+}
 
-            grouped[key].totalReward += reward;
-            grouped[key].transactions.push(item);
-        });
-
-        const result = Object.values(grouped);
-        return result;
-    }
-
-    const handleModalClose = () => {
-        setShowTransactionModal(!setShowTransactionModal)
-    }
-
-    return (
-        <Box>
-            {referralRecords && referralRecords.length > 0 ? <ReferralDataGrid
-                rows={referralType === 'Credited' ? mapGroupedData(referralRecords) : referralRecords}
-                columns={referralType === 'Credited' ? ReferralCreditedColumns : ReferralColumns}
-                width={'70vw'} /> : <p>No Referral Found</p>}
-
-            <Dialog open={showTransactionModal} onClose={handleModalClose} fullWidth maxWidth="md">
-                <DialogTitle>All Transactions</DialogTitle>
-                <DialogContent>
-                    <div style={{ height: 400, width: '800', marginTop: 16 }}>
-                        <ReferralDataGrid rows={transactionList}
-                            width={'600'}
-                            columns={ReferralColumns} />
-                    </div>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleModalClose}>Cancel</Button>
-                </DialogActions>
-            </Dialog>
-        </Box>
-
-    );
-};
-
-export default ReferralTransactions;
+export default ReferralTransactions
--- src/services/kyc.service.ts
+++ src/services/kyc.service.ts
@@ -1,115 +1,104 @@
-import { BaseService } from "./base.service";
+import { BaseService } from './base.service'
 import api1 from './apis/api1'
 
 export class KycService extends BaseService {
-
-    async verifyDocument(doccode: string, kycid: string) {
-
-        let url = `/api/kyc/documents/${kycid}/${doccode}/verify`
-        try {
-            let payload = {
-                "kycId": kycid,               // KYC ID for this document
-                "documentCode": doccode  // Document Code
-            }
-            const { data } = await api1.post(url, payload);
-            return data
-        }
-        catch (err) {
-            console.log(err)
-
-        }
+  async verifyDocument(doccode: string, kycid: string) {
+    let url = `/api/kyc/documents/${kycid}/${doccode}/verify`
+    try {
+      let payload = {
+        kycId: kycid, // KYC ID for this document
+        documentCode: doccode, // Document Code
+      }
+      const { data } = await api1.post(url, payload)
+      return data
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    async unverifyDocument(doccode: string, kycid: string) {
-
-        let url = `/api/kyc/documents/${kycid}/${doccode}/unVerify`
-        try {
-            let payload = {
-                "kycId": kycid,               // KYC ID for this document
-                "documentCode": doccode  // Document Code
-            }
-            const { data } = await api1.post(url, payload);
-            return data
-        }
-        catch (err) {
-            console.log(err)
-
-        }
+  async unverifyDocument(doccode: string, kycid: string) {
+    let url = `/api/kyc/documents/${kycid}/${doccode}/unVerify`
+    try {
+      let payload = {
+        kycId: kycid, // KYC ID for this document
+        documentCode: doccode, // Document Code
+      }
+      const { data } = await api1.post(url, payload)
+      return data
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-
-    async changeKycStatus(status: string, kycid: string) {
-        let url = `/api/kyc/kyc/kycId/${kycid}/kycStatus/${status}`
-        try {
-            const { data } = await api1.post(url, {});
-            return data
-        }
-        catch (err) {
-            console.log(err)
-        }
+  async changeKycStatus(status: string, kycid: string) {
+    let url = `/api/kyc/kyc/kycId/${kycid}/kycStatus/${status}`
+    try {
+      const { data } = await api1.post(url, {})
+      return data
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    async createComment(payload: any) {
-        let url = `/api/kyc/comments`
-        try {
-            let data = api1.post(url, payload)
-            return data
-        }
-        catch (err) {
-            console.log(err)
-        }
+  async createComment(payload: any) {
+    let url = `/api/kyc/comments`
+    try {
+      let data = api1.post(url, payload)
+      return data
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    //@ts-ignore
+  //@ts-ignore
 
-    async getComment(kyc_id: any): Promise<Array<{
-        commentId: string;
-        commentText: string;
-        commentDate: string; // ISO format
-        user: string;
-        kycId: string;
-    }>> {
-        let url = `/api/kyc/comments`
-        try {
-            let data = api1.get(url)
-            return data
-        }
-        catch (err) {
-            console.log(err)
-            return null as any
-        }
+  async getComment(kyc_id: any): Promise<
+    Array<{
+      commentId: string
+      commentText: string
+      commentDate: string // ISO format
+      user: string
+      kycId: string
+    }>
+  > {
+    let url = `/api/kyc/comments`
+    try {
+      let data = api1.get(url)
+      return data
+    } catch (err) {
+      console.log(err)
+      return null as any
     }
+  }
 
-
-    //@ts-ignore
-    async getCharges(souceCountry, destinationCountry, amount, segment) {
-        let url = `/api/charges/service/filter?sendingCountry=SA&receivingCountry=${destinationCountry}&amount=${amount}&marketSegment=${segment}`
-        try {
-            let data = await api1.get(url)
-            return (data) as any
-
-        }
-        catch (err) { console.log(err)
-        }
+  //@ts-ignore
+  async getCharges(souceCountry, destinationCountry, amount, segment) {
+    let url = `/api/charges/service/filter?sendingCountry=SA&receivingCountry=${destinationCountry}&amount=${amount}&marketSegment=${segment}`
+    try {
+      let data = await api1.get(url)
+      return data as any
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    async getReferralCreditedTransactions(referrerApplicantId:string) {
-        let url = `/api/kyc/referral-transaction/credited/${referrerApplicantId}`
-        try {
-            let data = await api1.get(url)
-            return (data) as any
-        }
-        catch (err) { console.log(err)
-        }
+  async getReferralCreditedTransactions(referrerApplicantId: string) {
+    const url = `api/kyc/referral-transaction/transactions/${referrerApplicantId}`
+    try {
+      let data = await api1.get(url)
+      return data as any
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-     async getReferralRedeemedTransactions(applicantId:string) {
-        let url = `/api/kyc/referral-transaction/redeemed/${applicantId}`
-        try {
-            let data = await api1.get(url)
-            return (data) as any
-        }
-        catch (err) { console.log(err)
-        }
+  async getReferralRedeemedTransactions(applicantId: string) {
+    let url = `/api/kyc/referral-transaction/redeemed/${applicantId}`
+    try {
+      let data = await api1.get(url)
+      return data as any
+    } catch (err) {
+      console.log(err)
     }
+  }
 }
