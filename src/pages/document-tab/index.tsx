b2857ccb7bb031614fd64b4b460b9aec0289e48f
siddhant.kaushik <siddhant.kaushik@impronics.co.za>
2025-08-14 17:56:38 +0530

    changes in the view document

--- src/pages/document-tab/index.tsx
+++ src/pages/document-tab/index.tsx
@@ -25,8 +25,8 @@ const DocumentsListComponent = ({ documentRecords }: { documentRecords: any }) =
       headerClassName: 'super-app-theme--header',
       renderCell: (params: any) => (
         <IconButton onClick={() => handleViewDocument(params.row.docUrl?.replace(
-  "http://64.227.139.142",
-  "https://api.impronics.com"
+  "http://164.90.252.179/",
+  "https://api.impronics.com/uat/files/"
 ))} color="primary">
           <VisibilityIcon />
         </IconButton>
