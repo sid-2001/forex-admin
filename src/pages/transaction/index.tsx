afcbcb2b1cb5bbbcc3dfbe5aac6eaca48f99bbad
siddhant.kaushik <siddhant.kaushik@impronics.co.za>
2025-09-19 12:39:08 +0530

    changes in the build

--- src/pages/transaction/index.tsx
+++ src/pages/transaction/index.tsx
@@ -895,6 +895,7 @@ transactionType == 'inwards'?<>
             loading={getLoadingState()}
             columnVisibilityModel={columnVisibilityModel}
             onColumnVisibilityModelChange={setColumnVisibilityModel}
+            //@ts-ignore
             loading={isLoading}
             slots={{
               loadingOverlay: LoaderUI.LoadingOverlay,
