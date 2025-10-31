03138b9349efa5229072be51bb7a9ff111f4b225
payal.garg <garg.payal@impronics.co.za>
2025-10-31 17:12:39 +0530

    added pagination changes

--- src/pages/audit-log/index.tsx
+++ src/pages/audit-log/index.tsx
@@ -4,9 +4,15 @@ import { Box, Typography, FormControl, InputLabel, MenuItem, Select } from '@mui
 import { AuditService } from '@/services/audit.services'
 import LoaderUI from '@/components/loader/loader'
 import moment from 'moment'
+import { GridColDef, GridToolbar, GridPaginationModel, GridFilterModel } from '@mui/x-data-grid'
 
 const AuditLogTable: React.FC = () => {
   const [auditLogData, setAuditLogData] = useState([])
+  const [paginationModel, setPaginationModel] = React.useState<GridPaginationModel>({
+    page: 0,
+    pageSize: 20,
+  })
+  const [rowCount, setRowCount] = useState(0)
   const [logType, setlogType] = useState('transaction_audit_log')
   const [isLoading, setIsLoading] = useState(false)
   const auditLogService = new AuditService()
@@ -21,18 +27,31 @@ const AuditLogTable: React.FC = () => {
 
   useEffect(() => {
     fetchAuditListingData()
-  }, [logType])
+  }, [logType, paginationModel])
 
   const fetchAuditListingData = useCallback(async () => {
     try {
       setIsLoading(true)
-      const response = await auditLogService.getAuditLogsListing(logType)
+      const { page, pageSize } = paginationModel
+      const response = await auditLogService.getAuditLogsListing(logType, page, pageSize)
       setAuditLogData(response.data)
+      setRowCount(response?.totalItems)
+      // setPaginationModel({
+      //   page: 0,
+      //   pageSize: response?.totalPages,
+      // })
+
       setIsLoading(false)
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error)
     }
-  }, [logType])
+  }, [logType, paginationModel])
+
+  // handle page or pageSize change
+  const handlePaginationChange = (newModel: GridPaginationModel) => {
+    console.log(newModel, '---------------')
+    setPaginationModel(newModel)
+  }
 
   const columns = [
     {
@@ -141,12 +160,16 @@ const AuditLogTable: React.FC = () => {
           }}
           columns={columns}
           rows={auditLogData || []}
-          initialState={{
-            pagination: {
-              paginationModel: { pageSize: 20, page: 0 },
-            },
-          }}
-          pageSizeOptions={[10]}
+          // initialState={{
+          //   pagination: {
+          //     paginationModel: { pageSize: 20, page: 0 },
+          //   },
+          // }}
+          rowCount={rowCount}
+          paginationMode="server"
+          paginationModel={paginationModel}
+          onPaginationModelChange={handlePaginationChange}
+          pageSizeOptions={[10, 20, 30, 40, 50]}
           loading={isLoading}
           slots={{
             loadingOverlay: LoaderUI.LoadingOverlay, // custom loader
--- src/services/audit.services.ts
+++ src/services/audit.services.ts
@@ -2,8 +2,8 @@ import { BaseService } from './base.service'
 import api1 from './apis/api1'
 
 export class AuditService extends BaseService {
-  async getAuditLogsListing(tableName: string): Promise<any> {
-    const url = `/api/transactions/auditLog/logs?tableName=${tableName}&page=1&size=20&sortBy=event_time&sortDir=desc`
+  async getAuditLogsListing(tableName: string, page: number, pagesize: number): Promise<any> {
+    const url = `/api/transactions/auditLog/logs?tableName=${tableName}&page=${page}&size=${pagesize}&sortBy=event_time&sortDir=desc`
     try {
       const { data } = await api1.get(url)
       return data
