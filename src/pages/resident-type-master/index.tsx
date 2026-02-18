f768e2fdc62c2f0b165fa283c88a96cf33918e70
siddhant.kaushik <siddhant.kaushik@impronics.co.za>
2026-02-18 20:12:52 +0530

    changes

--- src/pages/resident-type-master/index.tsx
+++ src/pages/resident-type-master/index.tsx
@@ -153,18 +153,22 @@ export default function ResidentTypeMaster() {
         async (pos) => {
           const liveAudit:any = await getLiveAuditData(pos.coords.latitude, pos.coords.longitude)
           if (liveAudit) {
+            //@ts-ignore
             await submitPayload(liveAudit )
           } else {
+             //@ts-ignore
             await submitPayload(audit)
           }
         },
         async (_) => {
           console.warn('Location denied, using fallback.')
+           //@ts-ignore
           await submitPayload(audit)
         },
         { timeout: 5000 },
       )
     } else {
+       //@ts-ignore
       await submitPayload(audit)
     }
   }
