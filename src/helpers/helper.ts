acc6b1241a01c634f1392aa03e6b67514db73e11
Rakshit Kumar <rakshit.kumar@impronics.co.za>
2026-09-11 14:49:30 +0530

    enhancement requierd on web pannel

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -1,22 +1,24 @@
-import moment from 'moment-timezone';
-import { LocalStorageService } from './local-storage-service';
-import dayjs from 'dayjs';
+import moment from 'moment-timezone'
+import { LocalStorageService } from './local-storage-service'
+import dayjs from 'dayjs'
 
 export class HelperService {
   local_service = new LocalStorageService()
   roundToTwoFixed(num: any) {
-    return (Math.round(num * 100) / 100).toFixed(2);
+    return (Math.round(num * 100) / 100).toFixed(2)
   }
 
   convertDateAndTime(date: any) {
-    return moment(date).tz("Asia/Kolkata").format("DD-MM-YYYY HH:mm:ss");
+    // need to manege in other coridor its right now dubai
+    // moment(date).tz("Asia/Kolkata").format("DD-MM-YYYY HH:mm:ss");
+    return moment(date).tz('Asia/Dubai').format('DD-MM-YYYY HH:mm:ss')
   }
 
   checkUserHasPermission(module: string, permission: string) {
-    const permission_granted = this.local_service.get_staff_access()?.modules.find((item:any) => item.moduleName === module);
-    return permission_granted?.access[permission] ? true : false;
+    const permission_granted = this.local_service.get_staff_access()?.modules.find((item: any) => item.moduleName === module)
+    return permission_granted?.access[permission] ? true : false
   }
-   formatTableDate = (dateString: string) => {
+  formatTableDate = (dateString: string) => {
     if (!dateString) return ''
     const storedConfig = localStorage.getItem('countryConfig')
     let format = 'YYYY-MM-DD'
@@ -27,8 +29,4 @@ export class HelperService {
     }
     return dayjs(dateString).format(format.toUpperCase())
   }
-
-
 }
-
-
